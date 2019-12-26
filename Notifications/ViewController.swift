0c17ea2116a0d91090eb50563e63e94a74ab13bc
Alejandro Robles <alejandro_r1704@hotmail.com>
2019-12-26 09:23:41 -0600

    Project created

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Omar Alejandro Robles Altamirano on 26/12/19.
+//  Copyright © 2019 Alejandro Robles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
